ec6de1d1a4c69dec18667917f2cf30d0105985c3
racheldoyle-sys <rachel.doyle@hennozzles.com>
2025-12-20 00:50:36 -0500

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,677 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    state_description: "",
+    state_color: "#e6e6e6",
+    state_hover_color: "#a5b9c4  ",
+    state_url: "",
+    border_size: "2.5",
+    all_states_inactive: "yes",
+    all_states_zoomable: "no",
+    location_description: "",
+    location_color: "#3076bc",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: "45",
+    location_type: "marker",
+    location_image_source: "",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#fff",
+    label_hover_color: "#fff",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "yes",
+    hide_eastern_labels: "yes    ",
+    manual_zoom: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    zoom_out_incrementally: "no",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    back_image: "no",
+    initial_back: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "./map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    border_hover_color: ""
+  },
+  state_specific: {
+    HI: {
+      zoomable: "no",
+      name: "Hawaii"
+    },
+    AK: {
+      zoomable: "no",
+      name: "Alaska"
+    },
+    FL: {
+      zoomable: "no",
+      name: "Florida"
+    },
+    NH: {
+      zoomable: "no",
+      name: "New Hampshire"
+    },
+    VT: {
+      zoomable: "no",
+      name: "Vermont"
+    },
+    ME: {
+      zoomable: "no",
+      name: "Maine"
+    },
+    RI: {
+      zoomable: "no",
+      name: "Rhode Island"
+    },
+    CT: {
+      zoomable: "no",
+      name: "Connecticut"
+    },
+    MA: {
+      zoomable: "no",
+      name: "Massachusetts"
+    },
+    NY: {
+      zoomable: "no",
+      name: "New York"
+    },
+    PA: {
+      zoomable: "no",
+      name: "Pennsylvania"
+    },
+    NJ: {
+      zoomable: "no",
+      name: "New Jersey"
+    },
+    DE: {
+      zoomable: "no",
+      name: "Delaware"
+    },
+    MD: {
+      zoomable: "no",
+      name: "Maryland"
+    },
+    VA: {
+      zoomable: "no",
+      name: "Virginia"
+    },
+    WV: {
+      zoomable: "no",
+      name: "West Virginia"
+    },
+    DC: {
+      zoomable: "no",
+      name: "District of Columbia"
+    },
+    OH: {
+      zoomable: "no",
+      name: "Ohio"
+    },
+    IN: {
+      zoomable: "no",
+      name: "Indiana"
+    },
+    IL: {
+      zoomable: "no",
+      name: "Illinois"
+    },
+    WI: {
+      zoomable: "no",
+      name: "Wisconsin"
+    },
+    MI: {
+      zoomable: "no",
+      name: "Michigan"
+    },
+    NC: {
+      zoomable: "no",
+      name: "North Carolina"
+    },
+    SC: {
+      zoomable: "no",
+      name: "South Carolina"
+    },
+    TN: {
+      zoomable: "no",
+      name: "Tennessee"
+    },
+    KY: {
+      zoomable: "no",
+      name: "Kentucky"
+    },
+    GA: {
+      zoomable: "no",
+      name: "Georgia"
+    },
+    AL: {
+      zoomable: "no",
+      name: "Alabama"
+    },
+    MS: {
+      zoomable: "no",
+      name: "Mississippi"
+    },
+    AR: {
+      zoomable: "no",
+      name: "Arkansas"
+    },
+    LA: {
+      zoomable: "no",
+      name: "Louisiana"
+    },
+    OK: {
+      zoomable: "no",
+      name: "Oklahoma"
+    },
+    MO: {
+      zoomable: "no",
+      name: "Missouri"
+    },
+    KS: {
+      zoomable: "no",
+      name: "Kansas"
+    },
+    IA: {
+      zoomable: "no",
+      name: "Iowa"
+    },
+    NE: {
+      zoomable: "no",
+      name: "Nebraska"
+    },
+    MN: {
+      zoomable: "no",
+      name: "Minnesota"
+    },
+    SD: {
+      zoomable: "no",
+      name: "South Dakota"
+    },
+    ND: {
+      zoomable: "no",
+      name: "North Dakota"
+    },
+    WY: {
+      zoomable: "no",
+      name: "Wyoming"
+    },
+    MT: {
+      zoomable: "no",
+      name: "Montana"
+    },
+    UT: {
+      zoomable: "no",
+      name: "Utah"
+    },
+    CO: {
+      zoomable: "no",
+      name: "Colorado"
+    },
+    AZ: {
+      zoomable: "no",
+      name: "Arizona"
+    },
+    NV: {
+      zoomable: "no",
+      name: "Nevada"
+    },
+    OR: {
+      zoomable: "no",
+      name: "Oregon"
+    },
+    WA: {
+      zoomable: "no",
+      name: "Washington"
+    },
+    CA: {
+      zoomable: "no",
+      name: "California"
+    },
+    ID: {
+      zoomable: "no",
+      name: "Idaho"
+    },
+    TX: {
+      zoomable: "no",
+      name: "Texas"
+    },
+    NM: {
+      zoomable: "no",
+      name: "New Mexico"
+    },
+    GU: {
+      hide: "yes",
+      zoomable: "no",
+      name: "Guam"
+    },
+    VI: {
+      hide: "yes",
+      zoomable: "no",
+      name: "Virgin Islands"
+    },
+    PR: {
+      hide: "yes",
+      zoomable: "no",
+      name: "Puerto Rico"
+    },
+    MP: {
+      hide: "yes",
+      zoomable: "no",
+      name: "Northern Mariana Islands"
+    },
+    AS: {
+      hide: "yes",
+      zoomable: "no",
+      name: "American Samoa"
+    }
+  },
+  locations: {
+    "0": {
+      name: "1st Due Fire Training",
+      lat: "35.0078",
+      lng: "97.0929"
+    },
+    "1": {
+      name: "Bearers of the Oath",
+      lat: "32.1574",
+      lng: "-82.9071"
+    },
+    "2": {
+      name: "Big Fire, Big Water",
+      lng: "-82",
+      lat: "28.5"
+    },
+    "3": {
+      name: "Build your Culture",
+      lat: "43",
+      lng: "-84.5"
+    },
+    "4": {
+      name: "Down and Dirty Training",
+      lat: "35",
+      lng: "-79.0193"
+    },
+    "5": {
+      name: "FIRECORE Training",
+      lat: "27",
+      lng: "-82"
+    },
+    "6": {
+      name: "Firefighter TDC",
+      lat: "43.7844",
+      lng: "-88.7879"
+    },
+    "7": {
+      name: "Flow and Vent",
+      lat: "40.0583",
+      lng: "-74.4057"
+    },
+    "8": {
+      name: "Love the Job Training",
+      lat: "40.5",
+      lng: "-78"
+    },
+    "9": {
+      name: "Misfits Training Foundation",
+      lat: "39",
+      lng: "-86.5"
+    },
+    "10": {
+      name: "On Scene Training Association",
+      lat: "41",
+      lng: -74.759
+    },
+    "11": {
+      name: "Tactical Resource Fire Training, LLC",
+      lat: "41.6032",
+      lng: "-73.0877"
+    },
+    "12": {
+      name: "The Citizens Fireman",
+      lat: "35.5175",
+      lng: "-86.5804"
+    },
+    "13": {
+      name: "The Grab Factory",
+      lat: "25.6648",
+      lng: "-81.2"
+    },
+    "14": {
+      name: "Triad Rescue International",
+      lat: "31.",
+      lng: "-98.5"
+    },
+    "15": {
+      name: "Big Fire, Big Water",
+      lat: "33.84°",
+      lng: "-81.16°"
+    }
+  },
+  labels: {
+    NH: {
+      parent_id: "NH",
+      x: "932",
+      y: "183",
+      width: 45
+    },
+    VT: {
+      parent_id: "VT",
+      x: "883",
+      y: "243",
+      width: 45
+    },
+    RI: {
+      parent_id: "RI",
+      x: "932",
+      y: "273",
+      width: 45
+    },
+    NJ: {
+      parent_id: "NJ",
+      x: "883",
+      y: "273",
+      width: 45
+    },
+    DE: {
+      parent_id: "DE",
+      x: "883",
+      y: "303",
+      width: 45
+    },
+    MD: {
+      parent_id: "MD",
+      x: "932",
+      y: "303",
+      width: 45
+    },
+    DC: {
+      parent_id: "DC",
+      x: "884",
+      y: "332",
+      width: 45
+    },
+    MA: {
+      parent_id: "MA",
+      x: "932",
+      y: "213",
+      width: 45
+    },
+    CT: {
+      parent_id: "CT",
+      x: "932",
+      y: "243",
+      width: 45
+    },
+    HI: {
+      parent_id: "HI",
+      x: 305,
+      y: 565
+    },
+    AK: {
+      parent_id: "AK",
+      x: "113",
+      y: "495"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "773",
+      y: "510"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "893",
+      y: "85"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "815",
+      y: "158"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "786",
+      y: "210"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "790",
+      y: "282"
+    },
+    WV: {
+      parent_id: "WV",
+      x: "744",
+      y: "270"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "700",
+      y: "240"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "650",
+      y: "250"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "600",
+      y: "250"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "575",
+      y: "155"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "784",
+      y: "326"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "655",
+      y: "340"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "548",
+      y: "368"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "548",
+      y: "293"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "718",
+      y: "405"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "760",
+      y: "371"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "680",
+      y: "300"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "655",
+      y: "405"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "550",
+      y: "435"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "600",
+      y: "405"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "525",
+      y: "210"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "506",
+      y: "124"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "460",
+      y: "360"
+    },
+    TX: {
+      parent_id: "TX",
+      x: "425",
+      y: "435"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "305",
+      y: "365"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "445",
+      y: "290"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "420",
+      y: "225"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "413",
+      y: "160"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "416",
+      y: "96"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "300",
+      y: "180"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "280",
+      y: "95"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "320",
+      y: "275"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "223",
+      y: "260"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "205",
+      y: "360"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "140",
+      y: "235"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "100",
+      y: "120"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "130",
+      y: "55"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "200",
+      y: "150"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "79",
+      y: "285"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "663",
+      y: "185"
+    },
+    PR: {
+      parent_id: "PR",
+      x: "620",
+      y: "545"
+    },
+    GU: {
+      parent_id: "GU",
+      x: "550",
+      y: "540"
+    },
+    VI: {
+      parent_id: "VI",
+      x: "680",
+      y: "519"
+    },
+    MP: {
+      parent_id: "MP",
+      x: "570",
+      y: "575"
+    },
+    AS: {
+      parent_id: "AS",
+      x: "665",
+      y: "580"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
